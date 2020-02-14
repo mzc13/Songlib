484de3613e5f9a1aa47e6d493e44decae1669930
Muhmmad <mzc13@scarletmail.rutgers.edu>
2020-02-14 13:10:25 -0500

    Project Initialization

---
+++ Songlib/src/songlib/app/Songlib.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package songlib.app;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author Abdulrahman Abdulrahman and Muhmmad Choudhary
+ *
+ */
+public class Songlib extends Application {
+
+	@Override
+	public void start(Stage mainStage) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
