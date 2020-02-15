3f3c0811ce1e5eda30b1273e20683cfce3310dbd
Muhmmad <mzc13@scarletmail.rutgers.edu>
2020-02-14 19:03:39 -0500

    Started added view and app components

--- Songlib/src/songlib/app/Songlib.java
+++ Songlib/src/songlib/app/Songlib.java
@@ -1,9 +1,9 @@
-/**
- * 
- */
 package songlib.app;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 /**
@@ -13,9 +13,16 @@ import javafx.stage.Stage;
 public class Songlib extends Application {
 
 	@Override
-	public void start(Stage mainStage) {
+	public void start(Stage mainStage) throws Exception {
 		// TODO Auto-generated method stub
-
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/songlib/view/index.fxml"));
+		GridPane root = (GridPane)loader.load();
+		Scene scene = new Scene(root);
+		mainStage.setScene(scene);
+		mainStage.setTitle("Songlib");
+		mainStage.setResizable(false);  
+		mainStage.show();
 	}
 
 	/**
---
+++ Songlib/src/songlib/common/Song.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package songlib.common;
+
+/**
+ * @author Abdulrahman Abdulrahman and Muhmmad Choudhary
+ *
+ */
+public class Song implements Comparable<Song> {
+	String name;
+	String artist;
+	String album;
+	String year;
+
+	/**
+	 * @param name - Required
+	 * @param artist - Required
+	 * @param album - Optional. If you send in null, it sets album to ""
+	 * @param year - Optional. Expected to always be an integer. If you send in
+	 * null, it sets year to ""
+	 */
+	public Song(String name, String artist, String album, String year) {
+		this.name = name;
+		this.artist = artist;
+		this.album = (album != null) ? album : "";
+		this.year = (year != null) ? year : "";
+	}
+	
+	@Override
+	public int compareTo(Song s) {
+		if(this.name.compareToIgnoreCase(s.name) != 0) {
+			return this.name.compareToIgnoreCase(s.name);
+		}else {
+			return this.artist.compareToIgnoreCase(s.artist);
+		}
+	}
+
+}
